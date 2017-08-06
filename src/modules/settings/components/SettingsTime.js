68afcefbb8e12be35dedce0dbb32e117b4c993e1
sufa bitton <sufabitton@sufas-imac.mynet>
2017-08-06 09:54:53 +0300

    time screen: add days input

--- src/modules/settings/components/SettingsTime.js
+++ src/modules/settings/components/SettingsTime.js
@@ -21,7 +21,16 @@ class SettingsTime extends Component {
 		super(props);
 		this.state = {
 			isDateTimePickerVisible: false,
-			time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+			time: new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}),
+			days:{
+				"א": false,
+				"ב": false,
+				"ג": false,
+				"ד": false,
+				"ה": false,
+				"ו": false,
+				"ש": false
+			}
 		};
 
 		this._handleDatePicked = this._handleDatePicked.bind(this);
@@ -47,6 +56,16 @@ class SettingsTime extends Component {
 						<TouchableHighlight style={styles.timeInput} underlayColor='#fff' onPress={this._showDateTimePicker}>
 							<Text style={styles.timeTxt}>
 								{this.state.time}
+							</Text>
+						</TouchableHighlight>
+					</View>
+				</View>
+				<View style={styles.daysContainer}>
+					<Text style={styles.txt}>ימי השארת הילד בגן</Text>
+					<View style={styles.daysInputContainer}>
+						<TouchableHighlight style={styles.dayInput} underlayColor='#fff' onPress={this._showDateTimePicker}>
+							<Text style={styles.dayTxt}>
+								א
 							</Text>
 						</TouchableHighlight>
 					</View>
@@ -122,6 +141,28 @@ const styles = StyleSheet.create({
 		color: 'grey',
 		fontSize: 19
 	},
+	daysContainer: {
+		marginTop: 45
+	},
+	dayInput: {
+		backgroundColor: 'rgb(227, 236, 253)',
+		borderRadius:5,
+		borderWidth: 1,
+		borderColor: '#fff',
+		paddingTop:10,
+		paddingBottom:10,
+		paddingLeft: 17,
+		paddingRight: 17
+	},
+	daysInputContainer: {
+		marginTop: 15,
+		flexDirection: 'row',
+		justifyContent: 'flex-end'
+	},
+	dayTxt: {
+		color: 'rgb(95, 135, 238)',
+		fontSize: 32
+	}
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(SettingsTime);
